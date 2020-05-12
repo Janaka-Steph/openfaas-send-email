ecf41b7c1f35cef2e5a3f0546eb5277a67288f5a
Janaka-Steph <rochestephane@yahoo.fr>
2020-05-12 17:18:06 +0100

    Make the function more generic

--- function/handler.js
+++ function/handler.js
@@ -6,7 +6,7 @@ const Yup = require('yup')
 const dotenv = require('dotenv')
 
 module.exports = async (event, context) => {
-  const {from_name, from_email, message} = event.body
+  const {from_name, from_email, to_email, message, subject} = event.body
 
   const schema = Yup.object().shape({
     from_name: Yup.string()
@@ -18,26 +18,35 @@ module.exports = async (event, context) => {
       .trim()
       .email('from_email is not valid')
       .required('from_email is required'),
+    to_email: Yup.string()
+      .trim()
+      .email('to_email is not valid')
+      .required('to_email is required'),
     message: Yup.string()
       .trim()
       .min(2, 'message must be at min 2 chars long')
       .max(2000, 'message must be at max 2000 chars long')
       .required('message is required'),
+    subject: Yup.string()
+      .trim()
+      .min(2, 'subject must be at min 2 chars long')
+      .max(200, 'subject must be at max 200 chars long')
+      .required('subject is required')
   })
 
   await schema.validate(event.body)
-  return sendEmail(from_name, from_email, message)
+  return sendEmail(from_name, from_email, to_email, message, subject)
 }
 
 
-const sendEmail = (from_name, from_email, message) => {
+const sendEmail = (from_name, from_email, to_email, message, subject) => {
   const config = fs.readFileSync('/var/openfaas/secrets/config', 'utf8')
   const {EMAIL, SMTP_GMAIL_PASS} = dotenv.parse(config)
 
   const mailOptions = {
     from: `"${from_name}" <${from_email}>`,
-    to: 'bitcoin-studio@protonmail.com',
-    subject: 'Bitcoin Studio Website Form',
+    to: to_email,
+    subject: subject,
     text: message
   }
 
